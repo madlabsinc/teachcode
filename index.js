13ec86e4eef07a8622590e1cbda3d6a2512efd89
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-12-22 12:35:17 +0530

    update path for fetchtask command :tada:

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const chalk = require('chalk');
 
 // Linking excercise files
 let initialize = require('./lib/commands/initializeTasks');
-let fetchTask = require('./lib/fetchTask');
+let fetchTask = require('./lib/commands/fetchTask');
 let submitTask = require('./lib/submitTask');
 let showKeys = require('./lib/showKeys');
 let showCommands = require('./lib/showCommands');
