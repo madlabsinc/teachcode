417ace14cd9a91ac8f2ae170c08993128f497dc1
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-01-10 18:02:49 +0530

    Add first test case :construction:

---
+++ test/main_test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const path = require('path'),
+{ exec } = require('child_process');
+
+const command = path.join(process.cwd(), 'index');
+
+describe('teach-code', () => {
+
+  it('executes without error', (done) => {
+    exec(command, (err) => {
+      if (err) {
+        throw err;
+      }
+      done();
+    });
+  });
+});
