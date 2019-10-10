02fc7c27866f637b203e64bce6fd6501cb1e2cc9
Sukhpal Saini <bdcorps@gmail.com>
2019-10-10 19:23:26 +0000

    feat: add -V and --version e2e test

--- test/index.js
+++ test/index.js
@@ -7,9 +7,13 @@ const test = require('ava');
 const rootCommand = path.join(process.cwd(), 'bin/index.js');
 
 test('shows up help message without any args', async t => {
-	const { stdout } = await execa(rootCommand);
-	t.snapshot(stdout);
+  const { stdout } = await execa(rootCommand);
+  t.snapshot(stdout);
 });
 
-
-
+['-V', '--version'].forEach(function(i){
+  test('shows version with arg ' + i, async t => {
+    const { stdout } = await execa(rootCommand, [i]);
+    t.snapshot(stdout);
+  });
+});
