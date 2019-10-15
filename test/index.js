b472a408d46ed09139763f7171a66842209ba963
Sukhpal Saini <bdcorps@gmail.com>
2019-10-15 15:00:43 +0000

    refactor: run linter

--- test/index.js
+++ test/index.js
@@ -12,12 +12,11 @@ test('shows up help message without any args', async t => {
 });
 
 test('shows version with arg -V', async t => {
-  const { stdout } = await execa(rootCommand, ["-V"]);
+  const { stdout } = await execa(rootCommand, ['-V']);
   t.snapshot(stdout);
 });
 
 test('shows version with arg --version', async t => {
-  const { stdout } = await execa(rootCommand, ["--version"]);
+  const { stdout } = await execa(rootCommand, ['--version']);
   t.snapshot(stdout);
 });
-
