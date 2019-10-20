c3fea6e66f0bf0a1a6d18d89b1ac1bd14ab75434
jamesjose03 <jamesjose03@gmail.com>
2019-10-20 23:28:59 +0530

    feat: Addition of tasks 16-18

---
+++ src/workspace/js/solutions/task17.js
@@ -0,0 +1,23 @@
+switch (new Date().getDay()) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+       day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+  }
+  console.log(day)
