e1021d241173d7676cace4ad6c9a28af066ac6e5
Rishabh Kalakoti <rishabh.kalakoti@gmail.com>
2019-12-10 02:34:46 +0530

    feat: e2e tests

--- test/index.js
+++ test/index.js
@@ -20,3 +20,23 @@ test('shows version with arg --version', async t => {
   const { stdout } = await execa(rootCommand, ['--version']);
   t.snapshot(stdout);
 });
+
+test('shows help with arg --help', async t => {
+  const { stdout } = await execa(rootCommand, ['--help']);
+  t.snapshot(stdout);
+});
+
+test('shows help with arg -h', async t => {
+  const { stdout } = await execa(rootCommand, ['-h']);
+  t.snapshot(stdout);
+});
+
+test('shows unknown commands with arg junkcmd', async t => {
+  const { stdout } = await execa(rootCommand, ['junkcmd']);
+  t.snapshot(stdout);
+});
+
+test('rejects promise due to error with arg -a', async t => {
+  const { stderr } = await execa(rootCommand, ['-a'], {reject:false});
+  t.snapshot(stderr);
+});
