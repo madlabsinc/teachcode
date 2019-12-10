662cae26d89c29cd82ddcf9e3d886c378da46fed
Rishabh Kalakoti <rishabh.kalakoti@gmail.com>
2019-12-10 13:45:27 +0530

    feat: e2e tests

--- test/index.js
+++ test/index.js
@@ -11,12 +11,20 @@ test('shows up help message without any args', async t => {
   t.snapshot(stdout);
 });
 
-test('shows version with arg -V', async t => {
-  const { stdout } = await execa(rootCommand, ['-V']);
+const macro = async (t, input) => {
+  const { stdout } = await execa(rootCommand, [input]);
   t.snapshot(stdout);
-});
+}
 
-test('shows version with arg --version', async t => {
-  const { stdout } = await execa(rootCommand, ['--version']);
-  t.snapshot(stdout);
+macro.title = (providedTitle = undefined, input) => providedTitle;
+
+test('shows version with arg --version', macro, '--version');
+test('shows version with arg -V', macro, '-V');
+test('shows help with arg -h', macro, '-h');
+test('shows help with arg --help', macro, '--help');
+test('shows command usage with unknown command', macro, 'junkcmd');
+
+test('rejects promise due to error with arg -a', async t => {
+  const { stderr } = await execa(rootCommand, ['-a'], {reject:false});
+  t.snapshot(stderr);
 });
