93fafb354f4ee1e5b61cf0d18fd41dd65e44a0fc
Rishabh Kalakoti <rishabh.kalakoti@gmail.com>
2019-12-29 21:56:11 +0530

    test: e2e test for fetchtask (incorrect tests/ code)

---
+++ __e2e__/commands/fetchtask.test.js
@@ -0,0 +1,136 @@
+'use strict';
+
+const path = require('path');
+const execa = require('execa');
+const test = require('ava');
+const fs = require('fs');
+
+const rootCommand = path.join(process.cwd(), 'bin/index.js');
+const configFilePath = path.join(process.cwd(), 'config.json');
+
+// learning track python, javascript
+const learningTracksInfo = [
+  { trackName: 'Python', fileExtension: 'py' },
+  { trackName: 'JavaScript', fileExtension: 'js' },
+];
+
+const userDefaultConfig = {
+  learningTrack: '',
+  userName: 'testConfig',
+  taskCount: 0,
+  keys: [],
+  userSubmittedFiles: [],
+};
+
+const createUserConfig = (
+  trackName,
+  fileExtension,
+  keysNumber,
+  filesNumber,
+) => {
+  var userConfig = userDefaultConfig;
+  userConfig['learningTrack'] = trackName;
+  for (let i = 0; i < keysNumber; i++)
+    userConfig['keys'].push('testKey' + (i + 1));
+  for (let i = 0; i < filesNumber; i++)
+    userConfig['userSubmittedFiles'].push(
+      'task' + (i + 1) + '.' + fileExtension,
+    );
+  userConfig['taskCount'] = filesNumber;
+
+  return userConfig;
+};
+
+test.serial('no config file for fetchtask', async t => {
+  if (fs.existsSync(configFilePath)) fs.unlinkSync(configFilePath);
+  const { stdout } = await execa(rootCommand, ['fetchtask'], {
+    reject: false,
+  });
+  t.snapshot(stdout);
+});
+
+test.skip('incorrect key for fetchtask', async t => {
+  let snap = null;
+  t.assert(learningTracksInfo.length > 0);
+  for (let index in learningTracksInfo) {
+    const learningTrack = learningTracksInfo[index];
+    const trackName = learningTrack['trackName'];
+    const fileExtension = learningTrack['fileExtension'];
+    const userConfig = createUserConfig(trackName, fileExtension, 30, 30);
+    fs.writeFileSync(configFilePath, JSON.stringify(userConfig));
+    const { stdout } = await execa(rootCommand, ['fetchtask', 'testKey0']);
+    fs.unlinkSync(configFilePath);
+    if (snap) t.true(snap === stdout);
+    snap = stdout;
+  }
+  t.snapshot(snap);
+});
+
+test.serial('display completed task with fetchtask key', async t => {
+  t.assert(learningTracksInfo.length > 0);
+  for (var index = 0; index < learningTracksInfo; index += 1) {
+    const learningTrack = learningTracksInfo[index];
+    const trackName = learningTrack['trackName'];
+    const fileExtension = learningTrack['fileExtension'];
+    let userConfig = createUserConfig(trackName, fileExtension, 6, 5);
+    fs.writeFileSync(configFilePath, JSON.stringify(userConfig));
+    const { stdout } = await execa(rootCommand, ['fetchtask', 'testKey2'], {
+      reject: false,
+    });
+    t.snapshot(stdout);
+    fs.unlinkSync(configFilePath);
+  }
+});
+
+// giving incorrect snapshot
+test.serial('display incomplete task with fetchtask key', async t => {
+  t.assert(learningTracksInfo.length > 0);
+  for (let index in learningTracksInfo) {
+    const learningTrack = learningTracksInfo[index];
+    const trackName = learningTrack['trackName'];
+    const fileExtension = learningTrack['fileExtension'];
+    let userConfig = createUserConfig(trackName, fileExtension, 6, 5);
+    fs.writeFileSync(configFilePath, JSON.stringify(userConfig));
+    const { stdout } = await execa(rootCommand, ['fetchtask', 'testKey6'], {
+      reject: false,
+    });
+    t.snapshot(stdout);
+    fs.unlinkSync(configFilePath);
+  }
+});
+
+test.skip('no more tasks available', async t => {
+  let snap = null;
+  t.assert(learningTracksInfo.length > 0);
+  for (let index in learningTracksInfo) {
+    const learningTrack = learningTracksInfo[index];
+    const trackName = learningTrack['trackName'];
+    const fileExtension = learningTrack['fileExtension'];
+    const userConfig = createUserConfig(trackName, fileExtension, 30, 30);
+    fs.writeFileSync(configFilePath, JSON.stringify(userConfig));
+    const { stdout } = await execa(rootCommand, ['fetchtask'], {
+      reject: false,
+    });
+    if (snap) t.assert(snap === stdout);
+    else snap = stdout;
+    fs.unlinkSync(configFilePath);
+  }
+  t.snapshot(snap);
+});
+
+// giving incorrect snapshots
+test.serial('display next task with fetchtask', async t => {
+  t.assert(learningTracksInfo.length > 0);
+  for (let index in learningTracksInfo) {
+    const learningTrack = learningTracksInfo[index];
+    const trackName = learningTrack['trackName'];
+    const fileExtension = learningTrack['fileExtension'];
+    const userConfig = createUserConfig(trackName, fileExtension, 6, 5);
+    fs.writeFileSync(configFilePath, JSON.stringify(userConfig));
+    const { stdout } = await execa(rootCommand, ['fetchtask'], {
+      reject: false,
+    });
+    t.snapshot(stdout);
+    fs.unlinkSync(configFilePath);
+  }
+});
