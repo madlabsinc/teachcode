16a19c49b17855b4136518cc900bdb8daa986756
Rishabh Kalakoti <rishabh.kalakoti@gmail.com>
2020-01-12 14:19:33 +0530

    feat: unit tests for commands

---
+++ src/commands/keys.test.js
@@ -0,0 +1,11 @@
+// const sinon = require('sinon');
+const test = require('ava');
+// const showBanner = require('node-banner');
+
+// const showkeys = require('keys');
+
+test.todo('no config file');
+
+test.todo('first task');
+
+test.todo('multiple tasks');
