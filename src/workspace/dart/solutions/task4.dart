c88c177371859c040c2715b60524d9eaebe8aa44
shan-shaji <abrahamshanshaji@gmail.com>
2022-01-16 15:34:05 +0530

    feat: add task4(string methods)

---
+++ src/workspace/dart/solutions/task4.dart
@@ -0,0 +1,7 @@
+void main() {
+  String str = 'Hey, this is Task 4 of Dart Path!';
+  print(str.length);
+  print(str.indexOf("Task"));
+  print(str.replaceAll("Dart", "Python"));
+  print(str.split(' '));
+}
